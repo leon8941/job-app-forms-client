5e7cce006e20e26d0da24754decfb1d12749a10d
Teck Onn <teckonn.miew@photobookworldwide.com>
2022-02-28 23:32:11 +0800

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,12 @@ import './App.css';
 import { FileUploader } from "react-drag-drop-files";
 import { fetchJobs, fetchLocations, fetchHeardFroms, submitJobApplication } from './api/jobApplications';
 
-interface IJob {
-  id: number,
-  title: string,
-}
+import { Modal } from './components/modal'
 
 function App() {
-  const [jobs, setJobs] = useState<IJob[]>([])
-  const [locations, setLocations] = useState([])
-  const [heardFroms, setHeardFroms] = useState([])
+  const [jobs, setJobs] = useState<{id: number, title: string}[]>([])
+  const [locations, setLocations] = useState<{id: number, name: string}[]>([])
+  const [heardFroms, setHeardFroms] = useState<{key: string, name: string}[]>([])
   const [showModal, setShowModal] = useState(false)
   const [previewFile, setPreviewFile] = useState<string | null>(null)
   const [previewFileName, setPreviewFileName] = useState<string | null>(null)
@@ -37,6 +34,10 @@ function App() {
       setHeardFroms(response)
     })
   }, [])
+
+  const onModalCloseHandler = () => {
+    setShowModal(false)
+  }
 
   const formik = useFormik({
     initialValues: {
@@ -116,54 +117,19 @@ function App() {
 
   const handleFileChange = (file: File) => {
     setFieldValue('file', file);
-
     setPreviewFile(URL.createObjectURL(file));
     setPreviewFileName(file.name);
   };
   
-  const basicModal = <>
-    <div
-      className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-    >
-      <div className="relative w-auto my-6 mx-auto max-w-3xl">
-        <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-          <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
-            <h3 className="text-3xl font-semibold">
-              Message
-            </h3>
-            <button
-              className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-              onClick={() => setShowModal(false)}
-            >
-              <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                ×
-              </span>
-            </button>
-          </div>
-          <div className="relative p-6 flex-auto">
-            <p className="my-4 text-blueGray-500 text-lg leading-relaxed">
-              Successfully submit job application
-            </p>
-          </div>
-          <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
-            <button
-              className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-              type="button"
-              onClick={() => setShowModal(false)}
-            >
-              Close
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-  </>
-
   return (
     <div>
       <>
-        {showModal ? basicModal : null}
+        {
+          showModal ? <Modal 
+            content='Successfully submit job application'
+            onCloseClick={onModalCloseHandler}
+          /> : null
+        }
         <div>
           <div className="md:grid md:grid-cols-3 md:gap-6">
             <div className="mt-5 md:mt-0 md:col-span-3">
@@ -473,7 +439,7 @@ function App() {
                       </div>
                       {
                         <div className="mb-3 text-xs text-red-500">
-                          {formik.errors.file ? <div>{formik.errors.file}</div> : null}  
+                          {formik.errors.file ? <div>{formik.errors.file}</div> : null}
                         </div>
                       }
                     </div>
